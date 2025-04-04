24cd2464266bd2f1d61e76cd94c71b0be3a68cb5
rruan16 <rodrigoruan16@hotmail.com>
2025-04-04 13:24:03 -0300

    add testes

---
+++ testes/testeAlgoritmos.c
@@ -0,0 +1,23 @@
+/******************************************************************************
+ * Arquivo: testeAlgoritmos.c
+ * 
+ * Descrição: Testes unitários para os algoritmos de ordenação implementados
+ *            em algoritmos.c. Este arquivo contém os casos de teste para
+ *            verificar a corretude e robustez das funções de ordenação.
+ * 
+ * Criadores: 
+ *            - CAIO MENDES COUTINHO E GARCIA - 2024007305
+ *            - DIEGO SILVA CORTEZ - 2024002480
+ *            - LUCAS ALEXANDRE DOS SANTOS BAESSO - 2024007172
+ *            - RAFAEL FERNANDO AURELIO RIBEIRO - 2024009426
+ *            - RODRIGO RUAN SOUZA VIANA - 2024004644
+ * 
+ * Data: 04/04/2025
+ ******************************************************************************/
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+
+int main() {
+    
+}
