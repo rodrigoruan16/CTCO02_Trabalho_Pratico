5aca94387d8c52302f09259293c4a4479e1b1c1b
Caio <d2024007305+1@unifei.edu.br>
2025-04-05 14:26:08 -0300

    accurate timing

--- testes/testeUtils.h
+++ testes/testeUtils.h
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <time.h>
+#include <sys/time.h>
 #include <assert.h>
 
 int runTimedSort(void (*sorting_algorithm)(int *vet, int tam), int *vet, int tam);
