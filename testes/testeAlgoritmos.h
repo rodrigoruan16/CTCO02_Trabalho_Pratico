8a7b9088f9787cbfed2ec69f9df0fe85ef35ee0c
rruan16 <rodrigoruan16@hotmail.com>
2025-04-10 19:00:12 -0300

    Add teste algoritmos .h

---
+++ testes/testeAlgoritmos.h
@@ -0,0 +1,20 @@
+/******************************************************************************
+ * Arquivo: testeAlgoritmos.c
+ * 
+ * Descrição: Testes unitários para os algoritmos de ordenação implementados
+ *            em algoritmos.c. Este arquivo contém os casos de teste para
+ *            verificar a corretude e robustez das funções de ordenação.
+ * 
+ * Criadores: 
+ *            - CAIO MENDES COUTINHO E GARCIA - 2024007305
+ *            - DIEGO SILVA CORTEZ - 2024002480
+ *            - LUCAS ALEXANDRE DOS SANTOS BAESSO - 2024007172
+ *            - RAFAEL FERNANDO AURELIO RIBEIRO - 2024009426
+ *            - RODRIGO RUAN SOUZA VIANA - 2024004644
+ * 
+ * Data: 04/04/2025
+ ******************************************************************************/
+
+typedef struct function function;
+
+void testSort(const void f(int *, int), char funcName[]);
