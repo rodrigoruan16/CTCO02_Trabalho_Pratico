d317c4d90d96bc17a1cc1519cf6d92a9ca40d46d
rodrigoruan16 <rodrigoruan16@hotmail.com>
2025-04-16 19:28:30 -0300

    t

--- testes/testeUtils.c
+++ testes/testeUtils.c
@@ -1,3 +1,21 @@
+/******************************************************************************
+ * Arquivo: testeUtils.c
+ *
+ * Descrição: Utilitários para os testes nos algoritmos.
+ *
+ * Criadores:
+ *            - CAIO MENDES COUTINHO E GARCIA - 2024007305
+ *            - DIEGO SILVA CORTEZ - 2024002480
+ *            - LUCAS ALEXANDRE DOS SANTOS BAESSO - 2024007172
+ *            - RAFAEL FERNANDO AURELIO RIBEIRO - 2024009426
+ *            - RODRIGO RUAN SOUZA VIANA - 2024004644
+ *
+ * Data: 16/04/2025
+ ******************************************************************************/
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/time.h>
+#include <assert.h>
 #include "testeUtils.h"
 
 struct SORT_INFORMATIONS
--- testes/testeUtils.h
+++ testes/testeUtils.h
@@ -1,8 +1,17 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <sys/time.h>
-#include <assert.h>
-
+/******************************************************************************
+ * Arquivo: testeUtils.h
+ *
+ * Descrição: Interfaces para utilitários para os testes nos algoritmos.
+ *
+ * Criadores:
+ *            - CAIO MENDES COUTINHO E GARCIA - 2024007305
+ *            - DIEGO SILVA CORTEZ - 2024002480
+ *            - LUCAS ALEXANDRE DOS SANTOS BAESSO - 2024007172
+ *            - RAFAEL FERNANDO AURELIO RIBEIRO - 2024009426
+ *            - RODRIGO RUAN SOUZA VIANA - 2024004644
+ *
+ * Data: 16/04/2025
+ ******************************************************************************/
 typedef struct SORT_INFORMATIONS SORT_INFORMATIONS;
 
 SORT_INFORMATIONS * runTimedSort(int* (*sorting_algorithm)(int *vet, int tam), int *vet, int tam);
