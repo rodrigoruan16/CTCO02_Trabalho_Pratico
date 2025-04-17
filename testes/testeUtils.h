856ad18a0ecbf8453a372d5509b24a611d621524
di-cortez <diegocortez96@hotmail.com>
2025-04-17 14:40:01 -0300

    Update testeUtils.h

--- testes/testeUtils.h
+++ testes/testeUtils.h
@@ -16,5 +16,6 @@ typedef struct SORT_INFORMATIONS SORT_INFORMATIONS;
 
 SORT_INFORMATIONS * runTimedSort(int* (*sorting_algorithm)(int *vet, int tam), int *vet, int tam);
 double getTime(SORT_INFORMATIONS *s);
+double getTimeCPU(SORT_INFORMATIONS *s);
 int getComparacoes(SORT_INFORMATIONS *s);
 int getTrocas(SORT_INFORMATIONS *s);
